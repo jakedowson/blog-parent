1e02713eb6967ab3e3fb15a323d0bcd23eeb711d
tomcat <tomcat@qq.com>
2020-07-31 11:12:50 +0800

    blog2

---
+++ blog-parent/blog-eureka/src/main/java/com/heima/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.heima.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by lenovo on 2020/7/1.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}
---
+++ blog-parent/blog-listener/src/main/java/com/itheima/ListenerApplication.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by lenovo on 2020/7/1.
+ */
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages = "com.itheima")
+@EntityScan("com.itheima.pojo")
+public class ListenerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ListenerApplication.class);
+    }
+}
